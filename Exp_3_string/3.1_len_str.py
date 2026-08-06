74b3bb262fa7ae648faa3c344ec1411aa945e423
MRehan-02 <pathanmohammadrehan02@gmail.com>
2026-08-06 13:03:36 +0530

    Remove Exp_3_string folder

--- Exp_3_string/3.1_len_str.py
+++
@@ -1,5 +0,0 @@
-s = input("Enter a string: ")
-count = 0
-for ch in s:
-    count = count + 1
-print("Length =", count)
--- Exp_3_string/3.9_first_last_char.py
+++
@@ -1,3 +0,0 @@
-s = input("Enter a string: ")
-print("First character:", s[0])
-print("Last character:", s[-1])
